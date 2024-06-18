af51c05913120144d310607181302a5c9b11a682
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-18 21:40:09 +0530

    add children div using arrays

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const parent = React.createElement("div",{id:"parent",},React.createElement("div",{id:"child"},
-  React.createElement("h1",{},"I am h1 tag")
-));
+// const parent = React.createElement("div", { id: "parent", },
+//   React.createElement("div", { id: "child" },
+//   React.createElement("h1", {}, "I am h1 tag")
+// )); 
+
+// to pass two childrens pass as array objects
+
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },[
+    React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am h1 tag'),
+    React.createElement('h2', {}, 'I am h2 tag')
+  ]),
+    React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am h1 tag'),
+    React.createElement('h2', {}, 'I am h2 tag')
+  ])]
+)
 
 console.log(parent)
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
+// root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
