59b7228d0fc1aec722a4e27c066da21a69a64cb8
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-19 13:07:00 +0530

    Building index.html using 'npx parcel index.html'

--- ep2/app.js
+++ ep2/app.js
@@ -1,25 +1,17 @@
-// const parent = React.createElement("div", { id: "parent", },
-//   React.createElement("div", { id: "child" },
-//   React.createElement("h1", {}, "I am h1 tag")
-// )); 
-
-// to pass two childrens pass as array objects
-
+import React from "react";
+import ReactDOM from 'react-dom/client'
 const parent = React.createElement(
   'div',
-  { id: 'parent' },[
-    React.createElement('div', { id: 'child' }, [
+  { id: 'parent' }, [
+  React.createElement('div', { id: 'child' }, [
     React.createElement('h1', {}, 'I am h1 tag'),
     React.createElement('h2', {}, 'I am h2 tag')
   ]),
-    React.createElement('div', { id: 'child' }, [
+  React.createElement('div', { id: 'child' }, [
     React.createElement('h1', {}, 'I am h1 tag'),
     React.createElement('h2', {}, 'I am h2 tag')
   ])]
-)
-
-console.log(parent)
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
-// root.render(heading);
+);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
