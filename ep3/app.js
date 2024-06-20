471856643b45d0a5a0ba19a52a2740abf9d51ad3
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-20 12:32:48 +0530

    add jsx and functional react  components

--- ep3/app.js
+++ ep3/app.js
@@ -3,5 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement("h1", { id: "heading" }, "React Element");
 
+// JSX => Babel tranpiled it to React.creteElement (object) => Html  element
+const jsxHeading = (
+  <h1 id="heading" className="head" tabIndex="2">
+  Namaste react
+  </h1>);
+
+const HeadingComponent = () => {
+   return <h1>Namaste React Function</h1>
+};
+
+const HeadingComponent2 = () => (
+    <div>
+    <h1>Namaste React Function</h1>
+    </div>
+);
+const fn = () => true;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
