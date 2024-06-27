0f61c95aad64b39b4de0beda788d01edd397bedc
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-27 19:40:11 +0530

    add Header.js

--- Hooks/src/components/Header.js
+++ Hooks/src/components/Header.js
@@ -1,24 +1,27 @@
 const Header = () => {
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img src="" alt="logo" className="logo"></img>
+    <div className='header'>
+      <div className='logo-container'>
+        <img src='' alt='logo' className='logo'></img>
       </div>
-      <div className="nav-items">
+      <div className='nav-items'>
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
           <li>
-            <img
-              src="https://static.vecteezy.com/system/resources/previews/027/381/351/original/shopping-cart-icon-shopping-trolley-icon-shopping-cart-logo-container-for-goods-and-products-economics-symbol-design-elements-basket-symbol-silhouette-retail-design-elements-vector.jpg"
-              alt="cart"
-              className="cart-icon"
-            ></img>
+            Home
+          </li>
+          <li>
+            About Us
+          </li>
+          <li>
+            Contact Us
+          </li>
+          <li>
+            <img src='https://static.vecteezy.com/system/resources/previews/027/381/351/original/shopping-cart-icon-shopping-trolley-icon-shopping-cart-logo-container-for-goods-and-products-economics-symbol-design-elements-basket-symbol-silhouette-retail-design-elements-vector.jpg'
+              alt='cart' className='cart-icon'></img>
           </li>
         </ul>
       </div>
     </div>
-  );
-};
-export default Header;
+  )
+}
+export default Header
