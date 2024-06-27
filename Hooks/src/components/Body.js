853745c6500b4c3967dafc6860d598a3b7a2fcca
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-27 23:41:10 +0530

    setListRest added to filter on certain conditions

--- Hooks/src/components/Body.js
+++ Hooks/src/components/Body.js
@@ -1,25 +1,25 @@
-import RestroCard from "./RestroCard";
-import resList from "../utils/mockData";
+import RestroCard from './RestroCard'
+import resList from '../utils/mockData'
 const Body = () => {
   return (
-    <div className="body">
-      <div className="filter">
+    <div className='body'>
+      <div className='filter'>
         <button
-          className="filter-btn"
+          className='filter-btn'
           onClick={() => {
-            console.Flog("Button-clicked");
+            Console.log('Button-clicked')
           }}
         >
           Top rated Restaurants
         </button>
       </div>
-      <div className="res-container">
-        {resList.map((res) => (
+      <div className='res-container'>
+        {resList.map(res => (
           <RestroCard key={res.info.id} resData={res} />
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Body;
+export default Body
