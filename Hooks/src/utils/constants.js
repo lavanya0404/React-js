2dc56f39344143374969985a3f44665da2b5a94a
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-27 20:18:59 +0530

    Named and default exports

--- Hooks/src/utils/constants.js
+++ Hooks/src/utils/constants.js
@@ -1,6 +1,7 @@
 const LOGO_URL =
-'https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg'
+  "https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg";
 
 export const CDN_URL =
-'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
-export default LOGO_URL
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export default LOGO_URL;
