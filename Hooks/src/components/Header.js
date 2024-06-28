6aba41273c7ee91f3c88094d0e7a2fff3f096d51
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-28 11:07:12 +0530

    Add toggle LogIn and LogOut button

--- Hooks/src/components/Header.js
+++ Hooks/src/components/Header.js
@@ -1,25 +1,22 @@
-import LOGO_URL from '../utils/constants'
+import LOGO_URL from "../utils/constants";
+import {useState} from "react";
 const Header = () => {
+  const[btnNameReact,setBtnNameReact
+  ]=useState("LogIn");
   return (
-    <div className='header'>
-      <div className='logo-container'>
-        <img src={LOGO_URL} alt='logo' className='logo'></img>
+    <div className="header">
+      <div className="logo-container">
+        <img src={LOGO_URL} alt="logo" className="logo"></img>
       </div>
-      <div className='nav-items'>
+      <div className="nav-items">
         <ul>
-          <li>
-            Home
-          </li>
-          <li>
-            About Us
-          </li>
-          <li>
-            Contact Us
-          </li>
-          <li>
-            <img src='https://static.vecteezy.com/system/resources/previews/027/381/351/original/shopping-cart-icon-shopping-trolley-icon-shopping-cart-logo-container-for-goods-and-products-economics-symbol-design-elements-basket-symbol-silhouette-retail-design-elements-vector.jpg'
-              alt='cart' className='cart-icon'></img>
-          </li>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button className="login" onClick={()=>{
+            btnNameReact =="LogIn" ? setBtnNameReact("LogOut") : setBtnNameReact("LogIn")
+          }}>{btnNameReact}</button>
         </ul>
       </div>
     </div>
