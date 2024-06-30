ff90aa05a9450fe3eb5393847ccb2e8427cd7aca
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-06-30 11:54:50 +0530

    Routes to different pages

--- ep7Router/src/app.js
+++ ep7Router/src/app.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {RouterProvider,createBrowserRouter} from 'react-router-dom'
 import Header from './components/Header'
 import Body from './components/Body'
-
+import About from './components/About'
+import Contact from './components/Contact'
+import ErrorElement from './components/ErrorElement'
 // not using any key (not acceptable) <<< index as key <<<<<<<<< unique (best practice)
 const AppLayout = () => {
   return (
@@ -12,6 +15,19 @@ const AppLayout = () => {
     </div>
   )
 }
-
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement:<ErrorElement/>
+  },
+  {
+    path: '/about',
+    element: <About />
+  },{
+    path :'/contact',
+    element: <Contact/>
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<AppLayout />)
+root.render(<RouterProvider router ={appRouter}/>)
