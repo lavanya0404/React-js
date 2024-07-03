0027301577830c8e16de5956278df389b525fb16
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-07-03 20:23:01 +0530

    Using props in classBased components

--- ep8-ClassBasedComponents/src/components/About.js
+++ ep8-ClassBasedComponents/src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About=()=>{
   return (
     <div> 
       <h1>About Us</h1>
-      <p>This is the about us page.</p>
+      <User/>
+      <UserClass name={'Lavanya Y M'} location={'Bengaluru'} contact ={'lavanya@gmail.com'} />
     </div>
   )
 }
---
+++ ep8-ClassBasedComponents/src/components/User.js
@@ -0,0 +1,10 @@
+const User = () => {
+  return(
+    <div className="user-card">
+      <h2>Name: Lavanya</h2>
+      <h3>Location: Bangalore</h3>
+      <h3>Contact: lavanyamaruthi0413@gmail.com</h3>
+    </div>
+  )
+}
+export default User;
