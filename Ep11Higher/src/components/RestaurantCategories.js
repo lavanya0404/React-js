695d1c1e0c94b62a7d17f4a8ab4c15d1e0e20c00
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-07-15 23:40:15 +0530

    Added "ADD" button and itemsList accordion

--- Ep11Higher/src/components/RestaurantCategories.js
+++ Ep11Higher/src/components/RestaurantCategories.js
@@ -1,17 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import ItemList from "./itemList"
 const RestaurantCategory = ({ data }) => {
-  console.log(data)
+  const [showList,setShowList] = useState(false);
+  const handleClick=()=>{
+  // console.log("handleClick")
+   setShowList(!showList);
+  }
   return (
     <div>
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 ">
-        <div className="flex justify-between">
+        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
+        {showList &&
         <ItemList items={data.itemCards} />
+        }
       </div>
     </div>
   )
--- Ep11Higher/src/components/itemList.js
+++ Ep11Higher/src/components/itemList.js
@@ -1,10 +1,40 @@
-const ItemList = ({items}) => {
+import { CDN_URL } from "../utils/constants"
+
+const ItemList = ({ items }) => {
   return (
     <div>
+      <div>
         <div>
-          category Items
+          {items.map((item) => (
+            <div
+              key={item.card.info.id}
+              className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between "
+            >
+              <div className="w-9/12 flex flex-col justify-between">
+                <div className="py-2">
+                  <span>{item.card.info.name}</span>
+                  <span>
+                    {" ₹ "}
+                    {item?.card?.info?.price / 100 ||
+                      item?.card?.info?.defaultPrice / 100}{" "}
+                  </span>
+                </div>
+                <p className="text-xs italic m-auto">{item.card.info.description}</p>
+              </div>
+              <div className="w-3/12 p-2">
+                <img
+                  src={CDN_URL + item.card.info.imageId}
+                  className="w-full rounded-md shadow-lg "
+                ></img>
+                <button className="p-2 m-3 bg-white shadow-lg mx-5 rounded-lg">
+                  ADD ➕
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
     </div>
   )
 }
-export default ItemList;
+export default ItemList
